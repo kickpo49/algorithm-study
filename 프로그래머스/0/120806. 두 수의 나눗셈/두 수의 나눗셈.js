9cb1291c2622af7042962ffe0164480d5f628bc8
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-09-27 18:06:32 +0900

    Update 두 수의 나눗셈.js

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,1 +1,7 @@
 const solution = (num1, num2) => Math.floor((num1/num2)*1000)
+
+// function solution(num1, num2) {
+//     return Math.trunc(num1 / num2 * 1000);
+// }
+// Math.floor()는 소수점을 내림한다.
+// Math.trunc()는 소수점을 버린다.
