8035efb917c485bc27cd5accb92b3211f17fc46f
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-09-30 16:27:30 +0900

    Update 각도기.js

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -11,3 +11,29 @@ function solution(angle) {
     }
     return answer;
 }
+
+
+// other solution 1
+function solution(angle) {
+    return [0, 90, 91, 180].filter(x => angle>=x).length;
+}
+
+// other solution 2
+function solution(angle) {
+    if (angle > 0 && angle < 90) return 1;
+    if (angle === 90) return 2;
+    if (angle > 90 && angle < 180) return 3;
+    if (angle === 180) return 4;
+    return 0;
+}
+
+// other solution 3
+function solution(angle) {
+  return angle < 90 ? 1 :
+         angle === 90 ? 2 :
+         angle < 180 ? 3 :
+         angle === 180 ? 4 : 0;
+}
+
+// 조건부 연산자 ? 설명
+// 조건1 ? 값1 : 조건2 ? 값2 : 조건3 ? 값3 : 기본값;
