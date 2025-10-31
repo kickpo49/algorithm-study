955a52fbb7ac6c433fd5bcbd67f8d25563bf632e
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-10-31 09:46:35 +0900

    [level 1] Title: 잔챙이 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
+++ 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
+# 문제가 잘못 출제됨 길이가 10cm 이하인 물고기의 수 조건을 빼고 LENGTH가 NULL인 경우로 답안 제출
 SELECT COUNT(*) AS FISH_COUNT
 FROM FISH_INFO
-WHERE LENGTH <= 10 OR LENGTH IS NULL;
-
+WHERE LENGTH IS NULL;
