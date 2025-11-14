84d532771c8f7a851ab83f55f71217408ad02dd7
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-11-14 15:00:52 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,20 @@
+-- 코드를 입력하세요
+SELECT
+    U.USER_ID,
+    U.NICKNAME,
+    CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, ' ', U.STREET_ADDRESS2) AS 전체주소,
+    CONCAT(
+        SUBSTRING(U.TLNO, 1, 3), '-',
+        SUBSTRING(U.TLNO, 4, 4), '-',
+        SUBSTRING(U.TLNO, 8, 4)
+    ) AS 전화번호
+FROM USED_GOODS_USER U
+WHERE U.USER_ID IN (
+    SELECT WRITER_ID
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*) >= 3
+)
+ORDER BY U.USER_ID DESC;
+
+# 문자열에서 일부분을 추출하는 함수 SUBSTRING(문자열, 시작위치, 길이)
