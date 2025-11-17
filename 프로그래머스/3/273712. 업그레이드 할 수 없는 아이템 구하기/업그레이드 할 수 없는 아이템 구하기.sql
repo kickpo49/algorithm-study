cdbdc2b4f326df7aa33e54799007020c51afa990
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-11-17 22:30:28 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 작성해주세요
+SELECT 
+    ITEM_ID,
+    ITEM_NAME,
+    RARITY
+FROM
+    ITEM_INFO
+WHERE 
+    ITEM_ID NOT IN (
+        SELECT PARENT_ITEM_ID
+        FROM ITEM_TREE
+        WHERE PARENT_ITEM_ID IS NOT NULL  -- NULL 값 제외
+    )
+ORDER BY 
+    ITEM_ID DESC;
