390e51f0bf9090c8565907703a3e05a835c20b93
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-12-03 15:06:56 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.py
@@ -0,0 +1,17 @@
+# Python3
+def solution(arr):
+    return ''.join(arr)
+# join() 메서드를 사용하여 배열의 모든 문자열을 빈 문자열('')로 연결합니다.
+
+                   
+# JavaScript                   
+# function solution(arr) {
+#     return arr.join('');
+# }
+                   
+# Java                  
+# class Solution {
+#     public String solution(String[] arr) {
+#         return String.join("", arr);
+#     }
+# }
