42dd7c8605725ff77039f36bcdf96f06ce5ff2db
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-12-06 12:44:39 +0900

    [level 0] Title: n의 배수, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -0,0 +1,22 @@
+# Python3
+def solution(num, n):
+    if num % n == 0:
+        return 1
+    else:
+        return 0
+    
+# def solution(num, n):
+#     return 1 if num % n == 0 else 0
+
+# JavaScript
+# function solution(num, n) {
+#   return num % n === 0 ? 1 : 0;
+# }
+
+
+# Java
+# class Solution {
+#     public int solution(int num, int n) {
+#         return (num % n == 0) ? 1 : 0;
+#     }
+# }
