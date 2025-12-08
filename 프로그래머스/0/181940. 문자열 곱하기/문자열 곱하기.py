3d19dcac7a7e80e3c92301f550da056ed0749b2a
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2025-12-08 22:02:09 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.py
@@ -0,0 +1,14 @@
+# Python3
+def solution(my_string, k):
+    return my_string * k
+
+# JavaScript
+# function solution(my_string, k) {
+#     return my_string.repeat(k);
+# }
+
+# Java
+# class Solution {
+#     public String solution(String my_string, int k) {
+#         return my_string.repeat(k);
+#     }
